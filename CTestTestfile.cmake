39434c8e5000046ca9b5fa646621fabda70f3c47
Jane Zou <75590646+jxlyn@users.noreply.github.com>
2022-07-18 18:54:13 -0400

    Update CTestTestfile.cmake

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,11 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: /Users/janezou/Documents/GitHub/cmake/Help/guide/tutorial/Step5
-# Build directory: /Users/janezou/Documents/GitHub/cmake/Help/guide/tutorial/Step5
-# 
+# Source directory: /Users/janezou/Documents/GitHub/cmake-step5
+# Build directory: /Users/janezou/Documents/GitHub/cmake-step5
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-
-
 
 add_test(Runs "/Users/janezou/Documents/GitHub/cmake-step5/Tutorial" "4096")
 set_tests_properties(Runs PROPERTIES  _BACKTRACE_TRIPLES "/Users/janezou/Documents/GitHub/cmake-step5/CMakeLists.txt;55;add_test;/Users/janezou/Documents/GitHub/cmake-step5/CMakeLists.txt;0;")
